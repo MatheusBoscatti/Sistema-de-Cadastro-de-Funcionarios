bd0b357a4c10a0a971c4c18a464da54fc1c65a24
MatheusBoscatti <boscatti660@gmail.com>
2023-06-16 13:01:14 -0300

    l

--- Pessoa.class.php
+++ Pessoa.class.php
@@ -1,6 +1,23 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-class Pessoa{
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilo.php">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<?php
+
+class Pessoa
+{
 
   public $Nome;
   public $DataNascimento;
@@ -8,26 +25,28 @@ class Pessoa{
   public $Sexo;
 
 
-  function __construct($Nome,$DataNascimento,$Email,$Sexo){
+  function __construct($Nome, $DataNascimento, $Email, $Sexo)
+  {
 
-    $this->Nome=$Nome;
-    $this->DataNascimento=$DataNascimento;
-    $this->Email=$Email;
-    $this->Sexo=$Sexo;
-}
+    $this->Nome = $Nome;
+    $this->DataNascimento = $DataNascimento;
+    $this->Email = $Email;
+    $this->Sexo = $Sexo;
+  }
 
 
 
-function VerPessoa(){
+  function VerPessoa()
+  {
 
+    echo "<div>";
 
     echo "<b>Nome: </b>{$this->Nome}<br />";
     echo "<b>DataNascimento</b>: {$this->DataNascimento}<br />";
     echo "<b>Email</b>: {$this->Email}<br />";
     echo "<b>Sexo</b>: {$this->Sexo}<br />";
-    }
 
-
-
+    echo "</div>";
+  }
 }
 ?>
--- estilo.php
+++ estilo.php
@@ -1,6 +1,14 @@
 <?php header("Content-type: text/css"); ?>
 
 body{
-    background-color: rgba(162, 197, 197, 0.867);
-    font-family: Arial, Helvetica, sans-serif;
+  background-color: rgba(162, 197, 197, 0.867);
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+
+div{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
