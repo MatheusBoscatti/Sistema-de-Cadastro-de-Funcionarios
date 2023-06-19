625db3cc573a3c6f89ee9baeab6f321f2eed99b0
MatheusBoscatti <boscatti660@gmail.com>
2023-06-19 13:39:33 -0300

    css div tirei

--- estilo.php
+++ estilo.php
@@ -4,11 +4,3 @@ body{
   background-color: rgba(162, 197, 197, 0.867);
   font-family: Arial, Helvetica, sans-serif;
 }
-
-
-div{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
